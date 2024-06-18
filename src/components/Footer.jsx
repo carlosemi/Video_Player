b3962d000b1f6640271ad7cc68fd35112e16094f
admin <admin@admins-MBP.lan>
2024-06-18 15:30:34 -0500

    Styling was improved

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,42 @@
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
-
   return (
-    <footer>
-        Footer
+    <footer className="footer mt-auto py-3 bg-dark text-white">
+      <Container>
+        <Row>
+          <Col md={4}>
+            <h5>About Us</h5>
+            <p>
+              We are a educational video sharing platform dedicated to bringing the best video content to our users. Our mission is to provide an easy-to-use platform for both creators and viewers.
+            </p>
+          </Col>
+          <Col md={4}>
+            <h5>Contact Us</h5>
+            <ul className="list-unstyled">
+              <li>Email: info@educationalplatform.com</li>
+              <li>Phone: +1 234 567 890</li>
+              <li>Address: 123 Video St, Film City, CA</li>
+            </ul>
+          </Col>
+          <Col md={4}>
+            <h5>Follow Us</h5>
+            <ul className="list-unstyled">
+              <li><a href="#" className="text-white">Facebook</a></li>
+              <li><a href="#" className="text-white">Twitter</a></li>
+              <li><a href="#" className="text-white">Instagram</a></li>
+              <li><a href="#" className="text-white">LinkedIn</a></li>
+            </ul>
+          </Col>
+        </Row>
+        <Row className="mt-3">
+          <Col className="text-center">
+            <p>&copy; 2024 Video Platform. All rights reserved.</p>
+          </Col>
+        </Row>
+      </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
